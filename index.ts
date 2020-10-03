d9bb202bf40e4a58314a2bad33d5b68055d152dd
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-10-03 23:15:13 +0100

    feat(screen): scale game screen dynamically

--- index.ts
+++ index.ts
@@ -2,9 +2,9 @@ import domready = require("domready");
 import {BaseAlign, Color, DisplayMode, Engine, FontUnit, Loader, TextAlign} from "excalibur";
 import Game from "./game/game";
 import {GlowLabelArgs} from "./glow-label";
+import {version} from "./package.json";
 import resources from "./resources";
 import Title from "./title/title";
-import {version} from "./package.json";
 
 export const labelDefaults: GlowLabelArgs = {
     fontFamily: "Knewave",
@@ -19,21 +19,30 @@ export const labelDefaults: GlowLabelArgs = {
 
 console.log(`Bunny Patrol v${version}`);
 
+const width = 320;
+const height = 240;
+
 domready(() => {
     const engine = new Engine({
-        viewport: {
-            width: 640,
-            height: 480
-        },
-        resolution: {
-            width: 320,
-            height: 240
-        },
+        viewport: {width, height},
+        resolution: {width, height},
         displayMode: DisplayMode.Fixed,
         antialiasing: false,
         suppressPlayButton: true,
         suppressHiDPIScaling: true
     });
+
+    const scale = (): void => {
+        const scaleFactor = Math.floor(Math.min(
+            window.innerWidth / width,
+            window.innerHeight / height
+        ));
+
+        engine.screen.viewport = {width: width * scaleFactor, height: height * scaleFactor};
+        engine.screen.applyResolutionAndViewport();
+    };
+
+    scale();
 
     const loader = new Loader(Object.values(resources));
 
@@ -42,4 +51,6 @@ domready(() => {
         engine.addScene("game", new Game(engine));
         engine.goToScene("title");
     });
+
+    window.addEventListener("resize", scale);
 });
