ec38d29b7abc2bf70ebde9866a5cfab18f874af2
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-10-04 01:59:07 +0100

    fix(screen): fix blurred upscale on Firefox
    The game is supposed to be upscaled using a nearest-neighbour algorithm but Firefox uses a bilinear
    filter. The root cause is that Firefox doesn't support `image-rendering: pixelated`. Work round this
    by checking if `image-rendering: pixelated` is supported and if not setting `image-rendering:
    crisp-edges` instead.
    
    See excaliburjs/Excalibur#1676

--- index.ts
+++ index.ts
@@ -32,6 +32,12 @@ domready(() => {
         suppressHiDPIScaling: true
     });
 
+    // Work around Firefox not supporting image-rendering: pixelated
+    // See https://github.com/excaliburjs/Excalibur/issues/1676
+    if (engine.canvas.style.imageRendering === "") {
+        engine.canvas.style.imageRendering = "crisp-edges";
+    }
+
     const scale = (): void => {
         const scaleFactor = Math.floor(
             Math.min(window.innerWidth / width, window.innerHeight / height)
